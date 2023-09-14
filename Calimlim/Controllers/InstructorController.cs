870261e9e54988292a56e6256f1ae367bb876b01
Carlos Miguel C. Calimlim <carlosmiguel.calimlim.cics@ust.edu.ph>
2023-09-14 22:04:46 +0800

    first commit

---
+++ Calimlim/Controllers/InstructorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calimlim.Models;
+using Calimlim.Models;
+
+namespace Calimlim.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+            {
+                new Instructor()
+                {
+                    InstructorId = 1,InstructorFirstName = "Gabriel",
+                    InstructorLastName = "Montano",
+                    Rank = Rank.Instructor,
+                    HiringDate = DateTime.Parse("2022-08-26"),
+                    IsTenured = true
+                },
+                new Instructor()
+                {
+                    InstructorId = 2,
+                    InstructorFirstName = "Paolo",
+                    InstructorLastName = "Santos",
+                    Rank = Rank.AssistantProfessor,
+                    HiringDate = DateTime.Parse("2022-08-07"),
+                    IsTenured = true
+                },
+                new Instructor()
+                {
+                    InstructorId = 3, InstructorFirstName = "Miguel",
+                    InstructorLastName = "Calimlim",
+                    Rank = Rank.Instructor, HiringDate =
+                    DateTime.Parse("2022-09-07"),
+                    IsTenured = true
+                }
+            };
+        public IActionResult Index()
+        {
+
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the student whose id matches the given id
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.InstructorId == id);
+
+            if (instructor != null)//was an instructor found?
+                return View(instructor);
+
+            return NotFound();
+        }
+    }
+}
+
+
---
+++ Calimlim/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calimlim.Models;
+using Calimlim.Models;
+
+namespace Calimlim.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> StudentList = new List<Student>
+            {
+                new Student()
+                {
+                    StudentId= 1,
+                    StudentFirstName = "Carlos Miguel",
+                    StudentLastName = "Calimlim",
+                    Course = Course.BSIT,
+                    AdmissionDate = DateTime.Parse("2021-08-20"),
+                    GPA = 1.6,
+                    Email = "carlosmiguelcalimlim@gmail.com"
+                },
+                new Student()
+                {
+                    StudentId= 2,StudentFirstName = "Jose Paolo",
+                    StudentLastName = "Santos",
+                    Course = Course.BSIS,
+                    AdmissionDate = DateTime.Parse("2021-05-01"),
+                    GPA = 1.6,
+                    Email = "josepaolo@gmail.com"
+                },
+                new Student()
+                {
+                    StudentId= 3,
+                    StudentFirstName = "Isaac Clyde",
+                    StudentLastName = "Bernal",
+                    Course = Course.BSCS,
+                    AdmissionDate = DateTime.Parse("2021-02-23"),
+                    GPA = 1.6,
+                    Email = "isaacclyde@gmail.com"
+                }
+            };
+        public IActionResult Index()
+        {
+
+            return View(StudentList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the student whose id matches the given id
+            Student? student = StudentList.FirstOrDefault(st => st.StudentId == id);
+
+            if (student != null)//was an student found?
+                return View(student);
+
+            return NotFound();
+        }
+    }
+}
